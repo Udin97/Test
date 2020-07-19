e60b491657dd870d9f274d56646aad45fa57c97e
Udin said <nashtv61@gmail.com>
2020-07-19 20:50:16 +0800

    Change2

---
+++ Test1/ViewModel/RandomViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Test1.Models;
+
+namespace Test1.ViewModel
+{
+    public class RandomViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> customers { get; set; }
+    }
+}
